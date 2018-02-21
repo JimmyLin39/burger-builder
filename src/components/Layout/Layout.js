b4bfa897d820dd0e97d7950ffd615170d14df695
JimmyLin39 <jimmylin0309@email.com>
2018-02-20 22:44:44 -0800

    move sidedrawer floder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = ( props ) => (
   <Aux>
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import Logo from '../../components/Logo/Logo';
+import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './SideDrawer.css'
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux/Aux';
 
 const sideDrawer = (props) => {
   return (
